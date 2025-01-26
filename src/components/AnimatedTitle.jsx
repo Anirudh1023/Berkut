3c5a74525db3942dacd89cd42a39e2fb0aa6974a
Anirudh1023 <anirudh.bocha@students.iiit.ac.in>
2025-01-27 01:45:22 +0530

    a

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -13,28 +13,22 @@ const AnimatedTitle = memo(({ title, containerClass }) => {
       const titleAnimation = gsap.timeline({
         scrollTrigger: {
           trigger: containerRef.current,
-          start: "top bottom",
-          end: "bottom bottom",
+          start: "100 bottom",
+          end: "center bottom",
           toggleActions: "play none none reverse",
         },
       });
 
-      titleAnimation.fromTo(
+      titleAnimation.to(
         ".animated-word",
         {
-          opacity: 0,
-          y: 50, // Start slightly below original position
-          rotateX: 45, // Add initial rotation
-        },
-        {
           opacity: 1,
-          y: 0, // Animate to original position
-          rotateX: 0,
-          transform: "translate3d(0, 0, 0) rotateY(0deg)",
-          ease: "power2.out",
-          stagger: 0.05, // Slightly increased stagger for smoother effect
-          duration: 0.6, // Added duration for more controlled animation
-        }
+          transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
+          ease: "power2.inOut",
+          stagger: 0.02,
+          will: "transform opacity",
+        },
+        0
       );
     }, containerRef);
 
@@ -44,20 +38,17 @@ const AnimatedTitle = memo(({ title, containerClass }) => {
   return (
     <div
       ref={containerRef}
-      className={clsx(
-        "absolute bottom-20 left-0 w-full z-20 text-center",
-        containerClass
-      )}
+      className={clsx("animated-title will-change-transform", containerClass)}
     >
       {title.split("<br />").map((line, index) => (
         <div
           key={index}
-          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3 font-general md:text-7xl text-3xl text-white"
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3 font-general md:text-7xl text-3xl will-change-transform"
         >
           {line.split(" ").map((word, idx) => (
             <span
               key={idx}
-              className="animated-word inline-block opacity-0 will-change-transform"
+              className="animated-word will-change-transform"
               dangerouslySetInnerHTML={{ __html: word }}
             />
           ))}
